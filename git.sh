246f25a92db5f59fd30e0440d4160dd3241ebf4f
Beilong Tang <bt132@duke.edu>
2024-10-01 15:54:12 +0800

    update

--- git.sh
+++ git.sh
@@ -13,16 +13,16 @@ else
 fi
 
 ### check if there are files that are over 100 MB included in the repo
-found_large_file=false
-git ls-files --others --exclude-standard | while read -r file; do
-    if [ -f "$file" ]; then
-        file_size=$(du -sh "$file" | awk '{print $1}')
-        if [[ $file_size == *M && $(echo "${file_size//M/}" | bc) -gt 50 ]]; then
-            echo "Untracked file over 100 MB: $file_size - $file"
-            exit 1
-        fi
-    fi
-done || exit 1
+# found_large_file=false
+# git ls-files --others --exclude-standard | while read -r file; do
+#     if [ -f "$file" ]; then
+#         file_size=$(du -sh "$file" | awk '{print $1}')
+#         if [[ $file_size == *M && $(echo "${file_size//M/}" | bc) -gt 50 ]]; then
+#             echo "Untracked file over 100 MB: $file_size - $file"
+#             exit 1
+#         fi
+#     fi
+# done || exit 1
 # # exit 1
 # echo 111222
 git add .
